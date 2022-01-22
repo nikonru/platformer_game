3e073cf61427e975c7b4adc55620648a62e32ead
Slava2001 <slavakaplya20011501@gmail.com>
2022-01-22 17:51:22 +0300

    add submodule sfml

--- src/graphics/graphics_manager.cpp
+++ src/graphics/graphics_manager.cpp
@@ -1,5 +1,36 @@
 #include "graphics_manager.h"
+#include <SFML/Graphics.hpp>
 
- graphics_manager::graphics_manager() {
-     
- }
+#include <iostream>
+ 
+graphics_manager::graphics_manager() {
+}
+
+void graphics_manager::main_loop() {
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Game!");
+    
+    sf::Clock clock;
+    int frame_counter = 0;
+
+    sf::CircleShape circle(200);
+    circle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        if (frame_counter == 10) {
+            frame_counter = 0;
+            std::cout << "FPS " << (10 / clock.restart().asSeconds()) << std::endl;
+        }
+        frame_counter++;
+
+        window.clear(sf::Color(66, 145, 255)); //light blue
+        window.draw(circle);
+        window.display();  
+    }
+}
--- src/graphics/graphics_manager.h
+++ src/graphics/graphics_manager.h
@@ -4,6 +4,7 @@ class graphics_manager {
 private:
 public:
     graphics_manager();
+    void main_loop();
 };
 
 
