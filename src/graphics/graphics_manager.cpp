d7f3a133be0e53cd6f7c5de887255175311fe433
Slava2001 <slavakaplya20011501@gmail.com>
2022-01-29 20:00:10 +0300

    add subscriber

--- src/graphics/graphics_manager.cpp
+++ src/graphics/graphics_manager.cpp
@@ -1,43 +1,56 @@
 #include "graphics_manager.h"
-
+#define DEBUG
+#include "util.h"
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <memory>
- 
-graphics_manager::graphics_manager() {
-    manager = std::make_shared<Logic_manager>();
+
+Graphics_manager::Graphics_manager() {
+    _manager = std::make_shared<Logic_manager>();
 }
 
+void Graphics_manager::main_loop() {
+    _main_window = std::make_shared<sf::RenderWindow>(sf::VideoMode(400, 400), "Game!");
+    _main_window->setFramerateLimit(120);
 
-
-void graphics_manager::main_loop() {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Game!");
-    
     sf::Clock clock;
     int frame_counter = 0;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        if (frame_counter == 10) {
+    
+    while (_main_window->isOpen()) {
+        processing_events();
+       
+        if (frame_counter == 20) {
             frame_counter = 0;
-            std::cout << "FPS " << (10 / clock.restart().asSeconds()) << std::endl;
+            debug_print("FPS: %.2f\n", (20 / clock.restart().asSeconds()));
         }
         frame_counter++;
 
-        manager->update();
+        _manager->update();
 
-        auto actors = manager->get_actors();
+        auto actors = _manager->get_actors();
 
-        window.clear(sf::Color(66, 145, 255)); //light blue
+        _main_window->clear(sf::Color(66, 145, 255)); //light blue
         for (auto& a: actors->vector)
-             window.draw(a->get_sprite());
-        window.display();  
+             _main_window->draw(a->get_sprite());
+        _main_window->display();  
     }
+}
+
+void Graphics_manager::processing_events() {
+    sf::Event event;
+    while (_main_window->pollEvent(event)) {
+
+        for (event_callback cb :_subscribers_to_events[event.type]) {
+           cb(event);     
+        }
+
+        if (event.type == sf::Event::Closed) {
+            _main_window->close();
+        }
+    }
+}
+
+void Graphics_manager::set_event_callback(sf::Event::EventType type, event_callback cb) {
+    _subscribers_to_events[type].push_back(cb);
 }
--- src/graphics/graphics_manager.h
+++ src/graphics/graphics_manager.h
@@ -3,13 +3,22 @@
 #include "logic_manager.h"
 #include "physics_manager.h"
 
-class graphics_manager {
-private:
-    std::shared_ptr<Logic_manager> manager;
-public:
-    graphics_manager();
-    void main_loop();
-};
+#include <map>
 
+typedef void (*event_callback)(sf::Event e);
+
+class Graphics_manager {
+private:
+    std::shared_ptr<Logic_manager> _manager;
+    std::shared_ptr<sf::RenderWindow> _main_window;
+
+    std::map<sf::Event::EventType, std::vector<event_callback>> _subscribers_to_events;
+
+    void processing_events();
+public:
+    Graphics_manager();
+    void main_loop();
+    void set_event_callback(sf::Event::EventType type, event_callback cb);
+};
 
 
