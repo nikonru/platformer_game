6d32b89f7dda8f766b7ac4fd8f4b3704c37690e7
Slava2001 <slavakaplya20011501@gmail.com>
2022-01-29 19:55:18 +0300

    add util

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "graphics_manager.h"
+#define DEBUG
+#include "util.h"
 
 int main()
 {
+    debug_print("Enter\n");
     // TODO add input manager
     graphics_manager graphics;
     graphics.main_loop();
---
+++ src/util/util.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "cstdio"
+
+#ifdef DEBUG
+#define DEBUG_MODE 1
+#else
+#define DEBUG_MODE 0
+#endif
+
+#define debug_print(msg, ...) \
+do { \
+    if (DEBUG_MODE) { \
+        fprintf(stdout, "%s(): " msg, __func__, ##__VA_ARGS__); \
+    } \
+} while(0)
+
+#define debug_var(var) std::cout << #var << ": " << var << std::endl;
